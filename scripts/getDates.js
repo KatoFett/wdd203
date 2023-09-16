2ec8b1f1f66af49f071fd961bb0a938cae2df2d4
BuildTools <unconfigured@null.spigotmc.org>
2023-09-16 12:35:40 -0600

    Week 01 Course Home Page

---
+++ scripts/getDates.js
@@ -0,0 +1,5 @@
+// Copyright year
+document.getElementById('currentYear').innerText = new Date().getFullYear();
+
+// Last modified
+document.getElementById('lastModified').innerText = `Last Modified: ${document.lastModified}`;
